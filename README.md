7dd0ba66b3cb83adb20df31ed139a937d1ac5d3b
Dani <s.leslie94@gmail.com>
2022-05-12 11:23:38 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # calculator
+
+This is the calculator project from The Odin Project's Javascript Foundations course. I was tasked with building a functioning calculator using Javascript, HTML, and CSS.
+
+# Challenges
+
+- evaluate a single pair of numbers at a time
+- string together several operations 
+- add keyboard support  
+
+# Areas for Improvement
---
+++ script.js
@@ -0,0 +1,27 @@
+function add(x,y){
+return x+y;
+}
+
+function subtract(x,y){
+    return x-y;
+}
+
+function multiply(x,y){
+    return x*y;
+}
+
+function divide(x,y){
+    return x/y;
+}
+
+function operate(x, y, operator){
+if (operator === "+"){
+    return add(x,y);
+} else if (operator === "-"){
+    return subtract(x,y);
+} else if (operator === "*"){
+    return multiply(x,y);
+} else {
+    return divide(x,y);
+}
+}
