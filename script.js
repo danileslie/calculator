61b2480a2650204f1f8eac86ec738478088c2891
Dani <s.leslie94@gmail.com>
2022-05-16 19:45:04 -0400

    add functions so save numbers based on user input

--- script.js
+++ script.js
@@ -1,44 +1,91 @@
-let allButtons = document.querySelectorAll('div.buttons button');
+let numberButtons = document.querySelectorAll('div.buttons .number');
+let operatorButtons = document.querySelectorAll('div.buttons .operator');
 let display = document.querySelector('.display');
-let arr = [];
-let number;
 
-allButtons.forEach(button => {
-    button.addEventListener('click', buttonValue);
+let arr = [];
+let arr2 = [];
+let number;
+let number2;
+let xValueSet = false;
+
+function getXValue() {
+    
+        numberButtons.forEach(button => {
+            button.addEventListener('click', xValue);    
+        });      
+}
+getXValue();
+
+
+function getYValue() {
+    numberButtons.forEach(button => {
+    button.addEventListener('click', yValue);
+});
+}
+
+operatorButtons.forEach(button => {
+    button.addEventListener('click', operator);
 });
 
 function add(x,y){
 return x+y;
 }
 
-function subtract(x,y){
-    return x-y;
-}
+// function subtract(x,y){
+//     return x-y;
+// }
 
-function multiply(x,y){
-    return x*y;
-}
+// function multiply(x,y){
+//     return x*y;
+// }
 
-function divide(x,y){
-    return x/y;
-}
+// function divide(x,y){
+//     return x/y;
+// }
 
-function operate(x, y, operator){
-if (operator === "+"){
-    return add(x,y);
-} else if (operator === "-"){
-    return subtract(x,y);
-} else if (operator === "*"){
-    return multiply(x,y);
-} else {
-    return divide(x,y);
-}
-}
+// function operate(x, y, operator){
+// if (operator === "+"){
+//     return add(x,y);
+// } else if (operator === "-"){
+//     return subtract(x,y);
+// } else if (operator === "*"){
+//     return multiply(x,y);
+// } else {
+//     return divide(x,y);
+// }
+// }
  
-function buttonValue(x){
-    let value = Number(x.target.textContent);    
-    arr.push(value);
-    number = arr.join('');
-    display.textContent = number;
+function xValue(x){
+    if (!xValueSet){
+        let value = (x.target.textContent);    
+        arr.push(value);
+        number = Number(arr.join(''));
+        display.textContent = number;  
+    }          
 }
+
+function yValue(y){ 
+    let value = Number(y.target.textContent); 
+    arr2.push(value);
+    number2 = Number(arr2.join(''));
+    display.textContent = number2; 
+}
+
+function operator(){
+    operate();
+    getYValue();
+    xValueSet = true;
+}
+
+function operate(){
+    console.log(`Operate in progress. Current X value is ${number}`);
+    console.log(`Second operate in progress. Current Y value is ${number2}`);
+}
+
+function clearDisplay(){
+    display.value = '';
+    arr = [];
+    arr2 = [];
+}
+
 
