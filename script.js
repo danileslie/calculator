7b65152f2af0f2e699126924ce981995a512ae7a
Dani <s.leslie94@gmail.com>
2022-06-06 12:45:05 -0400

    correct rounding, clean code

--- script.js
+++ script.js
@@ -1,120 +1,3 @@
-// let numberButtons = document.querySelectorAll('div.buttons .number');
-// let operatorButtons = document.querySelectorAll('div.buttons .operator');
-// let equalButton = document.querySelector('.equal');
-// let clearButton = document.querySelector('.clear');
-// let display = document.querySelector('.display');
-
-// let arr = [];
-// let arr2 = [];
-// let number = 0;
-// let number2 = undefined;
-// let xValueSet = false;
-// let operator1 = null;
-// let operator2 = null;
-// let displayContent = 0;
-
-// function getXValue() {
-//         numberButtons.forEach(button => {
-//         button.addEventListener('click', xValue);    
-//         });      
-// }
-// getXValue();
-
-// function getYValue() {
-//     numberButtons.forEach(button => {
-//     button.addEventListener('click', yValue);
-// });
-// }
-
-// function operatorClicked(){
-//     for (let i = 0; i < operatorButtons.length; i++){
-//         operatorButtons[i].addEventListener('click', function(){
-//             if (operatorButtons[i].classList.contains('operator')){
-//                 let operator = operatorButtons[i].value;
-//                 insertOperator(operator);    
-//         }
-//     })
-// }
-// }
-
-// operatorClicked();
-
-// function xValue(x){
-//     if (!xValueSet){
-//         let value = (x.target.textContent);    
-//         arr.push(value);
-//         number = Number(arr.join(''));
-//         display.textContent = number;  
-//     }        
-// }
-
-// function yValue(y){ 
-//     let value = Number(y.target.textContent); 
-//     arr2.push(value);
-//     number2 = Number(arr2.join(''));
-//     display.textContent = number2; 
-// }
-
-// function insertOperator(operator){
-//     let solution = operate(number, number2, operator);  
-//     let result;      
-//     if (operator1 != null && operator2 === null){
-//         operator2 = operator1;
-//         result = operate(number, number2, operator2);
-//         display.textContent = result;
-//         number = result;  
-//     } else if (operator1 != null && operator2 != null){
-//     result = operate(number, number2, operator1);
-//     operator1 = operator;
-//     display.textContent = result;
-//     number = result;
-
-//     }
-//     else{
-//         operator1 = operator; 
-//     }
-//     arr2 = [];    
-//     console.log(`Result: ${result}, Number one: ${number}, Number 2: ${number2}`);
-// }
-
-// function operate(x, y, operator){
-//     xValueSet = true;
-//     getYValue();
-//     if (operator === '+'){
-//         return x + y;   
-//     } else if (operator === '-'){
-//         return x - y;
-//     } else if (operator === '*'){ 
-//         return x * y;
-//     } else {
-//         return x / y;       
-//     }   
-// }
-
-// // // equalButton.addEventListener('click', equal);
-
-// // clearButton.addEventListener('click', clearDisplay);
-
-// // // function updateDisplay(){
-// // //     display.innerText = displayContent; 
-// // // }
-// // // updateDisplay();
-
-// // function clearDisplay(){
-// //     arr = [];
-// //     arr2 = [];
-// //     number = 0;
-// //     number2 = undefined; 
-// //     updateDisplay();    
-// // }
-
-// // // equal button needs to run operate and then do nothing else until another operator + value is inputted
-// // // hitting operator button first makes it impossible to assign a starting x value
-
-// // // write if statement that either throws an error when operator button is clicked, or assigns the 
-// // // next input to the x value
-
-
 let firstNumber = undefined;
 let secondNumber = undefined;
 let firstOperator = undefined;
@@ -156,7 +39,6 @@ function buttonPress(){
 }
 buttonPress();
 
-
 function operate(x, y, operator){
     if (operator === '+'){
         return x + y;   
@@ -169,14 +51,17 @@ function operate(x, y, operator){
             return 'Nice try :)';
         } else {
             return x / y;
-        }
-               
+        }          
     }   
 }
 
 function updateDisplay(){
     let display = document.querySelector('.display');
     display.innerText = displayContent;
+    if (displayContent.length > 9){
+        // setting display content directly causes last number to be constantly replaced
+        display.innerText = displayContent.substring(0, 9);
+    }
 }
 updateDisplay();
 
@@ -214,7 +99,7 @@ function getOperator(operator){
         secondOperator = operator;
         secondNumber = displayContent;
         solution = operate(Number(firstNumber), Number(secondNumber), firstOperator);
-        displayContent = solution;
+        displayContent = getRound(solution);
         firstNumber = displayContent;
     }
     else if (firstOperator != undefined && secondOperator != undefined){
@@ -222,7 +107,7 @@ function getOperator(operator){
         secondNumber = displayContent;
         solution = operate(Number(firstNumber), Number(secondNumber), secondOperator);
         secondOperator = operator;
-        displayContent = solution;
+        displayContent = getRound(solution);
         firstNumber = displayContent;
     }
     else {
@@ -238,7 +123,7 @@ function getEqual(){
     // runs operate on equal press
     secondNumber = displayContent;
     solution = operate(Number(firstNumber), Number(secondNumber), firstOperator);
-    displayContent = solution;
+    displayContent = getRound(solution);
 }
 // resets after equal press
 firstNumber = displayContent;
@@ -267,3 +152,18 @@ function getBackSpace(){
     displayContent = displayContent.slice(0, -1);
     }
 }
+
+function getRound(solution){
+    let newSolution = solution.toString();
+    if (newSolution.includes('.')) {
+        newSolution = (Number(newSolution)).toFixed(3);
+        return newSolution;   
+    }
+    else if (newSolution.length > 9){
+        newSolution = solution.toPrecision(4);
+        return newSolution;   
+    } else {
+        return solution;
+    }
+}
+
